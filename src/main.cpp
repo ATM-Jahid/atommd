d06601ccad415eca2f42e1a6cd655e5e8ae5b09e
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-02-20 16:05:54 -0500

    Add vec_cal.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,26 @@
 int main(int argc, char **argv) {
 	std::cout << "hello world\n";
 
-	int a = 3, b = 4, sum, diff;
-	//vecAdd(sum, a, b);
-	std::cout << a << b << sum << '\n';
-	vecAdd(sum, a, b);
-	std::cout << a << b << sum << '\n';
-	vecSub(diff, a, b);
-	std::cout << a << b << diff << '\n';
+	vecR u, v, mul, div;
 
-	Prop abc;
-	abc.val = 20;
-	abc.sum = 40;
-	abc.sum2 = -2;
+	vecSet(u, 10, 20);
+	vecSet(v, -3, -9);
+	vecMul(mul, u, v);
+	vecDiv(div, u, v);
 
-	std::cout << abc.val << abc.sum2 << '\n';
+	std::cout << mul.x << div.y << '\n';
+	vecSelfScaleAdd(u, 3, v);
+	std::cout << u.x << u.y << '\n';
+
+	vecR p, q;
+	vecSetAll(p, -12);
+	vecSetZero(q);
+
+	vecR reg;
+	vecSetAll(reg, 20);
+	vecWrapAll(p, reg);
+
+	std::cout << p.x << p.y << '\n';
 
 	return 0;
 }
--- src/vec_cal.cpp
+++ src/vec_cal.cpp
@@ -1,7 +1,86 @@
-void vecAdd(int &sum, int &x, int &y) {
-	sum = x + y;
+#include "types.hpp"
+
+real Sqr(real x) {
+	return x * x;
 }
 
-void vecSub(int &diff, int &x, int &y) {
-	diff = x - y;
+real Cub(real x) {
+	return x * x * x;
+}
+
+void vecSet(vecR &vec, real cx, real cy) {
+	vec.x = cx;
+	vec.y = cy;
+}
+
+void vecSetAll(vecR &vec, real c) {
+	vecSet(vec, c, c);
+}
+
+void vecSetZero(vecR &vec) {
+	vecSetAll(vec, 0);
+}
+
+void vecAdd(vecR &sum, vecR u, vecR v) {
+	sum.x = u.x + v.x;
+	sum.y = u.y + v.y;
+}
+
+void vecSub(vecR &diff, vecR u, vecR v) {
+	diff.x = u.x - v.x;
+	diff.y = u.y - v.y;
+}
+
+void vecMul(vecR &mul, vecR u, vecR v) {
+	mul.x = u.x * v.x;
+	mul.y = u.y * v.y;
+}
+
+void vecDiv(vecR &div, vecR u, vecR v) {
+	div.x = u.x / v.x;
+	div.y = u.y / v.y;
+}
+
+real vecProd(vecR u) {
+	return u.x * u.y;
+}
+
+real vecDot(vecR u, vecR v) {
+	return (u.x * v.x) + (u.y * v.y);
+}
+
+real vecLenSq(vecR u) {
+	return vecDot(u, u);
+}
+
+void vecScale(vecR &vec, real s) {
+	vec.x *= s;
+	vec.y *= s;
+}
+
+void vecScaleCopy(vecR &vec, real s, vecR u) {
+	vec.x = s * u.x;
+	vec.y = s * u.y;
+}
+
+void vecScaleAdd(vecR &vec, vecR u, real s, vecR v) {
+	vec.x = u.x + s * v.x;
+	vec.y = u.y + s * v.y;
+}
+
+void vecSelfScaleAdd(vecR &u, real s, vecR v) {
+	vecScaleAdd(u, u, s, v);
+}
+
+void vecWrapAll(vecR &vec, vecR region) {
+	if (vec.x >= 0.5 * region.x) {
+		vec.x -= region.x;
+	} else if (vec.x < -0.5 * region.x) {
+		vec.x += region.x;
+	}
+	if (vec.y >= 0.5 * region.y) {
+		vec.y -= region.y;
+	} else if (vec.y < -0.5 * region.y) {
+		vec.y += region.y;
+	}
 }
