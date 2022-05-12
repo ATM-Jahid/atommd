e870928175ae23f4eedf943aabcff9f7e099e68e
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-05-12 02:02:02 -0400

    Use buffers and running avg for D_ab

--- examples/supercooled/dumdab.py
+++ examples/supercooled/dumdab.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from statistics import mean
 import matplotlib.pyplot as plt
 
 files = sys.argv[1:]
@@ -12,39 +13,49 @@ def extract(fileName, tint):
     ind = fileName.find('_')
     temp = fileName[ind+1:ind+4]
 
-    dabs = []
+    buffDiff = 5
+    buffLen = 50
+    buff = [0] * buffLen
 
-    ini = jar[50*873+9:51*873]
-    for jj in range(51, 151):
-        end = jar[jj*873+9:(jj+1)*873]
+    for b in range(0, 101-buffLen, buffDiff):
+        dabs = []
+        ini = jar[(50+b)*873+9:(51+b)*873]
 
-        xi = 0; xf = 0
-        yi = 0; yf = 0
-        zi = 0; zf = 0
+        for jj in range(1, 51):
+            end = jar[(50+b+jj)*873+9:(50+b+jj+1)*873]
 
-        for line in ini:
-            tmp = line.split()
-            if tmp[1] == '2':
-                xi += float(tmp[2])
-                yi += float(tmp[3])
-                zi += float(tmp[4])
+            xi = 0; xf = 0
+            yi = 0; yf = 0
+            zi = 0; zf = 0
 
-        for line in end:
-            tmp = line.split()
-            if tmp[1] == '2':
-                xf += float(tmp[2])
-                yf += float(tmp[3])
-                zf += float(tmp[4])
+            for line in ini:
+                tmp = line.split()
+                if tmp[1] == '2':
+                    xi += float(tmp[2])
+                    yi += float(tmp[3])
+                    zi += float(tmp[4])
 
-        mR = 1
-        num_atoms = 864
-        time = (jj-50)
-        Q = 1/0.8/0.2 * (mR*0.8+0.2)**2
-        f = Q / 6 / num_atoms / time
-        D = f * ((xf-xi)**2 + (yf-yi)**2 + (zf-zi)**2)
-        dabs.append(D)
+            for line in end:
+                tmp = line.split()
+                if tmp[1] == '2':
+                    xf += float(tmp[2])
+                    yf += float(tmp[3])
+                    zf += float(tmp[4])
 
-    plt.plot(dabs, color=plt.cm.turbo(tint), label=temp)
+            mR = 1
+            num_atoms = 864
+            Q = 1/0.8/0.2 * (mR*0.8+0.2)**2
+            f = Q / 6 / num_atoms / jj
+            D = f * ((xf-xi)**2 + (yf-yi)**2 + (zf-zi)**2)
+            dabs.append(D)
+
+        for ii in range(len(dabs)):
+            buff[ii] += dabs[ii]
+
+    numBuff = (100-buffLen) // buffDiff + 1
+    buff = [x/numBuff for x in buff]
+    runner = [mean(buff[:i+1]) for i in range(50)]
+    plt.plot(runner, color=plt.cm.turbo(tint), label=temp)
 
 tints = [i/10 for i in range(1, 10, 2)]
 for file, tint in zip(files, tints):
